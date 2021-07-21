ef2e4a69c932a304d3a1dd0446c49572b447c3c3
TarquiniusMajor <33639229+TarquiniusMajor@users.noreply.github.com>
2021-07-22 01:29:44 +0300

    Add files via upload

---
+++ bin/brain-even.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+<<<<<<< HEAD
+import { userNameQuery, gameProcess } from '../src/index.js';
+
+console.log('Welcome to the Brain Games!');
+const userName = userNameQuery();
+const gameStartMessage = 'Answer "yes" if the number is even, otherwise answer "no".';
+const questionFunction = () => Math.round(Math.random() * 1000 + 1);
+const messageFunction = (num) => `Question: ${num}\nYour answer: `;
+
+const rightAnswerFunction = (num) => (num % 2 === 0 ? 'yes' : 'no');
+
+gameProcess(gameStartMessage, questionFunction, messageFunction, rightAnswerFunction, userName);
