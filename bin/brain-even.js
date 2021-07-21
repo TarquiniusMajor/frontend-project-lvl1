f24cf719bc5ea92b98006f5de6234aface1ba461
TarquiniusMajor <barribassov@yandex.ru>
2021-07-21 18:04:03 +0300

    changing architecture to module with linter

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import evenGame from '../src/brain-even-core.js'
-import game from '../src/brain-games-core.js'
+import evenGame from '../src/brain-even-core.js';
+import game from '../src/brain-games-core.js';
 
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
--- src/brain-even-core.js
+++ src/brain-even-core.js
@@ -1,26 +1,24 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function evenGame() {
-    const num = Math.round(Math.random() * 1000 + 1);
-    const result = {
-        rightAnswer: null,
-        answer: null,
-        gameResult: null,
-        resultMessage: null
-    };
+  const num = Math.round(Math.random() * 1000 + 1);
+  const result = {
+    rightAnswer: null,
+    answer: null,
+    gameResult: null,
+    resultMessage: null,
+  };
 
-    result.rightAnswer = num % 2 === 0 ? 'yes' : 'no';
-    result.answer = readlineSync.question(`Question: ${num} \nYour answer: `);
-    result.gameResult = result.rightAnswer === result.answer;
-    if (result.gameResult) {
-        result.resultMessage = 'Correct!';
-    } else {
-        result.resultMessage = `${result.answer} is wrong answer ;(. Correct answer was ${result.rightAnswer}.`
-    }
+  result.rightAnswer = num % 2 === 0 ? 'yes' : 'no';
+  result.answer = readlineSync.question(`Question: ${num} \nYour answer: `);
+  result.gameResult = result.rightAnswer === result.answer;
+  if (result.gameResult) {
+    result.resultMessage = 'Correct!';
+  } else {
+    result.resultMessage = `${result.answer} is wrong answer ;(. Correct answer was ${result.rightAnswer}.`;
+  }
 
-    return result;
+  return result;
 }
 
 export default evenGame;
-
-
