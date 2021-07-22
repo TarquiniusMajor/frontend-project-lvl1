b0c878033a625d34693ca1bb3f7cde2d4e8a9376
TarquiniusMajor <barribassov@yandex.ru>
2021-07-22 23:30:44 +0300

    add brain-progression

---
+++ bin/games/brain-progression.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { userNameQuery, gameProcess } from '../../src/index.js';
+
+console.log('Welcome to the Brain Games!');
+const userName = userNameQuery();
+const gameStartMessage = 'What number is missing in the progression?';
+const questionFunction = () => {
+  const elCount = Math.round(Math.random() * 10 + 5);
+  const concealedNum = Math.round(Math.random() * elCount);
+  const result = [[], concealedNum];
+  const startNum = Math.round(Math.random() * 100);
+  const increment = Math.round(Math.random() * 10);
+  for (let i = 0; i < elCount; i += 1) {
+    const currentEl = startNum + increment * i;
+    result[0].push(`${currentEl}`);
+  }
+  return result;
+};
+
+const messageFunction = (arr) => {
+  const resArr = [];
+  for (let i = 0; i < arr[0].length; i += 1) {
+    if (i === arr[1]) {
+      resArr.push('..');
+    } else { resArr.push(arr[0][i]); }
+  }
+  const resString = resArr.join(' ');
+  return `Question: ${resString}\nYour answer: `;
+};
+
+const rightAnswerFunction = (arr) => arr[0][arr[1]];
+
+gameProcess(gameStartMessage, questionFunction, messageFunction, rightAnswerFunction, userName);
